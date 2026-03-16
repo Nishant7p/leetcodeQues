e9b2e67b92b8196bec0928df9a5fb93f9b5643f5
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-03-17 01:04:27 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 9.62%] - LeetPush

--- Unique-Paths.java
+++ Unique-Paths.java
@@ -1,25 +1,26 @@
 1class Solution {
-2    public int paths(int row,int col,int m,int n,int[][] dp){
-3        if(row>=m||col>=n){
-4            return 0;
-5        }
-6        if(row==m-1&&col==n-1){
-7            return 1;
+2    
+3       
+4    public int uniquePaths(int m, int n) {
+5        int[][] dp=new int[m][n];
+6       for(int i=0;i<m;i++){
+7            Arrays.fill(dp[i], -1);
 8        }
-9        if(dp[row][col]!=-1){
-10            return dp[row][col];
-11        }
-12        return dp[row][col]=paths(row+1,col,m,n,dp)+paths(row,col+1,m,n,dp);
-13
-14    }
-15    public int uniquePaths(int m, int n) {
-16        int[][] dp=new int[m][n];
-17       for(int i=0;i<m;i++){
-18            Arrays.fill(dp[i], -1);
-19        }
-20        int ans=paths(0,0,m,n,dp);
-21
-22        return ans;
-23        
-24    }
-25}
+9       for(int i=0;i<m;i++){
+10        dp[i][0]=1;
+11       }
+12       for(int i=0;i<n;i++){
+13        dp[0][i]=1;
+14       }
+15
+16
+17       
+18       for(int i=1;i<m;i++){
+19        for(int j=1;j<n;j++){
+20            dp[i][j]=dp[i][j-1]+dp[i-1][j];
+21        }
+22       }
+23       return dp[m-1][n-1];
+24        
+25    }
+26}
