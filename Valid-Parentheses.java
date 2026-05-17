026bb19f1ea2d8f7dae9265e608dcaefaefc9417
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-05-17 21:49:13 +0530

    [20] [Time Beats: 87.75%] [Memory Beats: 25.55%] - LeetPush

--- Valid-Parentheses.java
+++ Valid-Parentheses.java
@@ -1,24 +1,40 @@
 1class Solution {
 2    public boolean isValid(String s) {
 3        Stack<Character> st=new Stack<>();
-4        for(int i=0;i<s.length();i++){
-5            char c=s.charAt(i);
-6            if(c=='{'||c=='['||c=='('){
-7                st.push(c);
-8            }
-9            else{
-10                if(st.isEmpty()){
-11                    return false;
-12                }
-13                if((c==']'&&st.peek()!='[')||(c=='}'&&st.peek()!='{')||(c==')'&&st.peek()!='(')){
-14                    return false;
-15                }else{
-16                    st.pop();
-17                }
+4
+5        int n=s.length();
+6        int i=0;
+7        while(i<n){
+8            char c=s.charAt(i);
+9            if(c=='('||c=='{'||c=='['){
+10                st.add(c);
+11            }
+12            else{
+13            if((c==')'&&st.size()==0)||c==')'&&st.peek()!='('){
+14                return false;
+15            }
+16             if((c=='}'&&st.size()==0)||c=='}'&&st.peek()!='{'){
+17                return false;
 18            }
-19        }
-20        return st.isEmpty();
-21        
-22    }
-23}
+19             if((c==']'&&st.size()==0)||c==']'&&st.peek()!='['){
+20                return false;
+21            }
+22            st.pop();}
+23            i++;
 24
+25
+26
+27        }
+28
+29
+30
+31        if(st.isEmpty()){
+32            return true;
+33        }
+34        else{
+35            return false;
+36        }
+37       
+38    }
+39}
+40
