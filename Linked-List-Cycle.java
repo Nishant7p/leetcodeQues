532a18df3869c907f50966536054ef3fc0a81c3a
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-05-17 23:07:38 +0530

    [141] [Time Beats: 100.00%] [Memory Beats: 71.15%] - LeetPush

--- Linked-List-Cycle.java
+++ Linked-List-Cycle.java
@@ -11,23 +11,23 @@
 11 */
 12public class Solution {
 13    public boolean hasCycle(ListNode head) {
-14        if(head==null){
-15            return false;
-16        }
-17        if(head.next==null){
-18            return false;
-19        }
-20        ListNode s=head;
-21        ListNode f=head.next;
-22        
-23        while(f!=null&&f.next!=null&&f.next.next!=null){
-24            if(f==s){
-25                return true;
-26            }
-27            f=f.next.next;
-28            s=s.next;
+14        ListNode f=head;
+15        ListNode s=head;
+16
+17        while(f!=null&&f.next!=null&&f.next.next!=null){
+18            f=f.next.next;
+19            s=s.next;
+20            if(f==s){
+21                return true;
+22            }
+23
+24
+25
+26        }
+27        return false;
+28
 29
-30        }
-31        return false;
+30
+31        
 32    }
 33}
