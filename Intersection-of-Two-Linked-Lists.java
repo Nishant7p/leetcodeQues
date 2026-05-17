760b30952914b1b0e3edd9c5fa28720ad3ed7721
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-05-17 23:25:38 +0530

    [160] [Time Beats: 99.95%] [Memory Beats: 12.22%] - LeetPush

--- Intersection-of-Two-Linked-Lists.java
+++ Intersection-of-Two-Linked-Lists.java
@@ -10,52 +10,59 @@
 10 * }
 11 */
 12public class Solution {
-13    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-14        int c1=0;
-15        int c2=0;
-16        ListNode l1=headA;
-17        ListNode l2=headB;
-18        while(l1!=null){
-19            c1++;
-20            l1=l1.next;
+13    public ListNode getIntersectionNode(ListNode hA, ListNode hB) {
+14        int l1=0;
+15        int l2=0;
+16        ListNode t1=hA;
+17        ListNode t2=hB;
+18        while(t1!=null){
+19            l1++;
+20            t1=t1.next;
 21        }
-22        while(l2!=null){
-23            c2++;
-24            l2=l2.next;
-25        }
-26
-27        int a_s=0;
-28        if(c1<c2){
-29            a_s=1;
-30        }
-31        if(a_s==1){
-32            l1=headA;
-33            l2=headB;
-34            for(int i=0;i<c2-c1;i++){
-35                l2=l2.next;
+22
+23        while(t2!=null){
+24            l2++;
+25            t2=t2.next;
+26        }
+27        int d=Math.abs(l1-l2);
+28        t1=hA;
+29        t2=hB;
+30
+31        if(l1>l2){
+32
+33            
+34            for(int i=0;i<d;i++){
+35                t1=t1.next;
 36            }
 37
 38
-39        }
-40        else{
-41            l2=headB;
-42            l1=headA;
-43            for(int i=0;i<c1-c2;i++){
-44                l1=l1.next;
+39
+40
+41        }
+42        else{
+43            for(int i=0;i<d;i++){
+44                t2=t2.next;
 45            }
 46
-47
-48        }
-49        while(l1!=null&&l2!=null){
-50            if(l1==l2){
-51                return l1;
+47        }
+48
+49        while(t1!=null&&t2!=null){
+50            if(t1==t2){
+51                return t1;
 52            }
-53            l1=l1.next;
-54            l2=l2.next;
+53            t1=t1.next;
+54            t2=t2.next;
 55        }
-56        return null;
+56
 57
 58
-59        
-60    }
-61}
+59
+60
+61
+62        return null;
+63
+64
+65       
+66        
+67    }
+68}
