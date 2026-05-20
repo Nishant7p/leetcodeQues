f49c8122a4620bdf8f0b0a034c8f20e51e4d709e
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-05-20 06:56:35 +0530

    [2657] [Time Beats: 100.00%] [Memory Beats: 31.21%] - LeetPush

--- Find-the-Prefix-Common-Array-of-Two-Arrays.java
+++ Find-the-Prefix-Common-Array-of-Two-Arrays.java
@@ -1,39 +1,27 @@
 1class Solution {
 2    public int[] findThePrefixCommonArray(int[] A, int[] B) {
-3        int n=A.length;
-4        int[] c=new int[n];
-5        HashMap<Integer,ArrayList<Integer>> a=new HashMap<>();
-6        HashMap<Integer,ArrayList<Integer>> b=new HashMap<>();
-7        
+3        int n = A.length;
+4        int[] c = new int[n];
+5        int[] freq = new int[n + 1];
+6
+7        int x = 0;
 8
-9        ArrayList<Integer> aa=new ArrayList<>();
-10        ArrayList<Integer> bb=new ArrayList<>();
+9        for(int i = 0; i < n; i++){
+10            freq[A[i]]++;
 11
-12        for(int i=0;i<n;i++){
-13           
-14            aa.add(A[i]);
-15            bb.add(B[i]);
-16            a.put(i,new ArrayList<>(aa));
-17            b.put(i,new ArrayList<>(bb));
-18        
-19
-20        }
-21        for(int i=0;i<n;i++){
-22            int x=0;
-23             ArrayList<Integer> aaa=a.get(i);
-24             ArrayList<Integer> bbb=b.get(i);
-25
-26             for(int j=0;j<aaa.size();j++){
-27                if(bbb.contains(aaa.get(j))){
-28                    x++;
-29                }
-30             }
-31             c[i]=x;
-32
-33        }
-34        return c;
-35
-36
-37        
-38    }
-39}
+12            if(freq[A[i]] == 2){
+13                x++;
+14            }
+15
+16            freq[B[i]]++;
+17
+18            if(freq[B[i]] == 2){
+19                x++;
+20            }
+21
+22            c[i] = x;
+23        }
+24
+25        return c;
+26    }
+27}
