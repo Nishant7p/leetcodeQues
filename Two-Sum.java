06f18f8796a7009531ed83ceac8479c9e43bc720
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-05-25 19:49:42 +0530

    [1] [Time Beats: 45.94%] [Memory Beats: 88.73%] - LeetPush

--- Two-Sum.java
+++ Two-Sum.java
@@ -1,12 +1,16 @@
 1class Solution {
 2     public int[] twoSum(int[] nums, int target) {
-3        HashMap<Integer, Integer> map = new HashMap<>();
-4        for (int i = 0; i < nums.length; i++) {
-5            if (map.containsKey(target - nums[i])) {
-6                return new int[]{i, map.get(target - nums[i])};
-7            }
-8            map.put(nums[i], i);
-9        }
-10        return nums;
-11    }
-12}
+3        int[] ans=new int[2];
+4        HashMap<Integer,Integer> map=new HashMap<>();
+5        for(int i=0;i<nums.length;i++){
+6            if(map.containsKey(target-nums[i])){
+7                ans[0]=map.get(target-nums[i]);
+8                ans[1]=i;
+9
+10            }
+11            map.put(nums[i],i);
+12        }
+13        return ans;
+14       
+15    }
+16}
