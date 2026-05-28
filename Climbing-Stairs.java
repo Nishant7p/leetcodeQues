618341ac157164f19266e2398b5d8d3fc5daf3f3
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-05-29 02:02:30 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 52.72%] - LeetPush

--- Climbing-Stairs.java
+++ Climbing-Stairs.java
@@ -1,13 +1,18 @@
 1class Solution {
 2    public int climbStairs(int n) {
-3        int[] dp=new int[n+1];
-4        dp[0]=1;
-5        dp[1]=1;
-6
-7        for(int i=2;i<=n;i++){
-8            dp[i]=dp[i-1]+dp[i-2];
-9        }
-10        return dp[n];
-11       
-12    }
-13}
+3        
+4       
+5        int s1=1;
+6        int s2=1;
+7        int cur=1;
+8
+9        for(int i=2;i<=n;i++){
+10            cur=s1+s2;
+11            
+12            s2=s1;
+13            s1=cur;
+14        }
+15        return cur;
+16       
+17    }
+18}
