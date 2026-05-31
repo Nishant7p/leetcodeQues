9a6a2ca24974e89a245a94d1ddfd59993d4d458a
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-05-31 22:59:47 +0530

    [48] [Time Beats: 100.00%] [Memory Beats: 50.31%] - LeetPush

--- Rotate-Image.java
+++ Rotate-Image.java
@@ -1,26 +1,29 @@
 1class Solution {
 2    public void rotate(int[][] matrix) {
-3        int temp=0;
-4        for(int i=0;i<matrix.length;i++){
-5            for(int j=i+1;j<matrix.length;j++){
-6                temp=matrix[i][j];
+3        int n=matrix.length;
+4        for(int i=0;i<n;i++){
+5            for(int j=i+1;j<n;j++){
+6                int t=matrix[i][j];
 7                matrix[i][j]=matrix[j][i];
-8                matrix[j][i]=temp;
+8                matrix[j][i]=t;
 9
 10            }
 11        }
-12        
-13
-14        for(int i=0;i<matrix.length;i++){
-15            for(int j=0;j<matrix.length/2;j++){
-16                temp=matrix[i][j];
-17                matrix[i][j]=matrix[i][matrix.length-j-1];
-18                matrix[i][matrix.length-j-1]=temp;
-19
-20            }
-21        }
+12        for(int i=0;i<n;i++){
+13            for(int j=0;j<n/2;j++){
+14                int t=matrix[i][j];
+15                matrix[i][j]=matrix[i][n-j-1];
+16                matrix[i][n-1-j]=t;
+17
+18
+19            }
+20        }
+21
 22
-23        
-24    }
-25}
-26
+23
+24       
+25
+26        
+27    }
+28}
+29
