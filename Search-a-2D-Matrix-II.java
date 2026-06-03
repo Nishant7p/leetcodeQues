be09da5f766b0abdfee13c81879f76fd895c90c3
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-06-03 20:05:37 +0530

    [240] [Time Beats: 22.58%] [Memory Beats: 24.36%] - LeetPush

--- Search-a-2D-Matrix-II.java
+++ Search-a-2D-Matrix-II.java
@@ -20,25 +20,15 @@
 20    public boolean searchMatrix(int[][] matrix, int target) {
 21        int m=matrix.length;
 22        int n=matrix[0].length;
-23
-24        for(int i=0;i<m;i++){
-25            int s=matrix[i][0];
-26            int e=matrix[i][n-1];
-27            if(target>=s&&target<=e){
-28                if(BS(0,n-1,target,matrix[i])){
-29                    return true;
-30                }
-31            }
-32
-33
-34
-35
-36
-37        }
-38        return false;
-39
-40
-41
-42        
-43    }
-44}
+23        for(int i=0;i<m;i++){
+24            if(BS(0,n-1,target,matrix[i])){
+25                return true;
+26            }
+27
+28        }
+29        return false;
+30
+31       
+32        
+33    }
+34}
