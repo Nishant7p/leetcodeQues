1ef29fce8b62a2b154fcb20f66bc60e4b251dd09
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-06-03 19:59:49 +0530

    [73] [Time Beats: 98.32%] [Memory Beats: 79.02%] - LeetPush

--- Set-Matrix-Zeroes.java
+++ Set-Matrix-Zeroes.java
@@ -1,28 +1,35 @@
 1class Solution {
 2    public void setZeroes(int[][] matrix) {
-3        int[] r=new int[matrix.length];
-4        int[] c=new int[matrix[0].length];
-5        for(int i=0;i<matrix.length;i++){
-6            for(int j=0;j<matrix[0].length;j++){
-7                if(matrix[i][j]==0){
-8                    r[i]=1;
-9                    c[j]=1;
-10                    
-11                }
-12            }
-13            
+3        int m=matrix.length;
+4        int n=matrix[0].length;
+5        int[] row=new int[m];
+6        int[] col=new int[n];
+7        for(int i=0;i<m;i++){
+8            for(int j=0;j<n;j++){
+9                if(matrix[i][j]==0){
+10                    row[i]=1;
+11                    col[j]=1;
+12                }
+13            }
 14        }
-15
-16        for(int i=0;i<matrix.length;i++){
-17            for(int j=0;j<matrix[0].length;j++){
-18                if(r[i]==1||c[j]==1){
-19                    matrix[i][j]=0;
-20                    
-21
-22                }
-23            }
-24        }
-25        
-26    }
-27}
-28
+15        for(int i=0;i<m;i++){
+16            if(row[i]==1){
+17                for(int j=0;j<n;j++){
+18                    matrix[i][j]=0;
+19                }
+20            }
+21        }
+22         for(int i=0;i<n;i++){
+23            if(col[i]==1){
+24                for(int j=0;j<m;j++){
+25                    matrix[j][i]=0;
+26                }
+27            }
+28        }
+29        
+30
+31        
+32
+33    }
+34}
+35
