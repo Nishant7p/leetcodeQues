09618262044f5704ff0aaa5f7ed841a9900ac39d
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-06-09 00:55:14 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 22.99%] - LeetPush

--- Sort-Colors.java
+++ Sort-Colors.java
@@ -1,32 +1,35 @@
 1class Solution {
 2    public void sortColors(int[] nums) {
-3        int n=nums.length;
-4        int r=0;
-5        int w=0;
-6        int b=0;
+3        int c_0=0;
+4        int c_1=0;
+5        int c_2=0;
+6
 7        for(int i:nums){
 8            if(i==0){
-9                r++;
-10            }
-11            else if(i==1){
-12                w++;
-13            }
-14            else{
-15                b++;
-16            }
-17        }
-18        for(int i=0;i<r;i++){
-19            nums[i]=0;
-20        }
-21        for(int i=0;i<w;i++){
-22            nums[r+i]=1;
-23        }
-24        for(int i=0;i<b;i++){
-25            nums[r+w+i]=2;
-26        }
-27
-28        
+9                c_0++;
+10            }else if(i==1){
+11                c_1++;
+12            }
+13            else{
+14                c_2++;
+15            }
+16        }
+17        
+18        int n=nums.length;
+19        for(int i=0;i<c_0;i++){
+20            nums[i]=0;
+21
+22        }
+23        for(int i=0;i<c_1;i++){
+24            nums[i+c_0]=1;
+25        }
+26        for(int i=0;i<c_2;i++){
+27            nums[i+c_1+c_0]=2;
+28        }
 29
-30        
-31    }
-32}
+30       
+31        
+32
+33        
+34    }
+35}
