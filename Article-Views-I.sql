f3ff614802e7f8f42f7f83af2c1d9f93d2964f0a
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-06-18 22:38:57 +0530

    [1148] [Time Beats: 86.29%] - LeetPush

---
+++ Article-Views-I.sql
@@ -0,0 +1,41 @@
+1# Write your MySQL query statement below
+2
+3
+4Select distinct author_id as id from Views
+5where author_id=viewer_id
+6order by author_id asc;
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
