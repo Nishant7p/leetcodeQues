a418f4fcd2f3dd86b1152e2ef16e76b75a8bc46a
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-06-29 06:26:46 +0530

    [1967] [Time Beats: 75.75%] [Memory Beats: 74.57%] - LeetPush

--- Number-of-Strings-That-Appear-as-Substrings-in-Word.java
+++ Number-of-Strings-That-Appear-as-Substrings-in-Word.java
@@ -2,31 +2,20 @@
 2    
 3    public int numOfStrings(String[] patterns, String word) {
 4        
-5        HashSet<String> set=new HashSet();
-6       
-7        int n=word.length();
-8
-9        for(int i=0;i<n;i++){
-10            StringBuilder sb=new StringBuilder();
-11            for(int j=i;j<n;j++){
-12                sb.append(word.charAt(j));
-13                set.add(sb.toString());
-14            }
-15        }
-16        System.out.println(set);
-17        int ans=0;
+5       
+6        int ans=0;
+7
+8        for( String s:patterns){
+9            if(word.contains(s)){
+10                ans++;
+11            }
+12
+13        }
+14       
+15
+16
+17
 18
-19        for( String s:patterns){
-20            if(set.contains(s)){
-21                ans++;
-22            }
-23
-24        }
-25       
-26
-27
-28
-29
-30        return ans;
-31    }
-32}
+19        return ans;
+20    }
+21}
