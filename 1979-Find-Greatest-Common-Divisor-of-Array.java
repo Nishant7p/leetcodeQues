65c9fbc7b7d4a7ae15ebf553b5f1fb829424e95c
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-07-18 12:03:33 +0530

    [1979] [Time Beats: 100.00%] [Memory Beats: 41.68%] - LeetPush

---
+++ 1979-Find-Greatest-Common-Divisor-of-Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int gcd(int a,int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+    public int findGCD(int[] nums) {
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+            min=Math.min(nums[i],min);
+            max=Math.max(nums[i],max);
+        }
+        return gcd(min,max);
+
+        
+    }
+}
