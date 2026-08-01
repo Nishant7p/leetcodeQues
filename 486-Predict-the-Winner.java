c29825df734128b298b5a24c0afb63c139a8a6d1
Nishant Tomer <143290670+Nishant7p@users.noreply.github.com>
2026-08-01 15:03:47 +0530

    [486] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 486-Predict-the-Winner.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n=nums.length;
+        if(n%2==0){
+            return true;
+        }
+        if(n==1){
+            return true;
+        }
+        
+
+
+
+
+
+        return false;
+        
+    }
+}
